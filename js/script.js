18d3400e1a973695c8442984e8d6f7f089c281a2
Miranda Billue <mj@Mirandas-MBP.attlocal.net>
2022-08-22 14:04:51 -0400

    name addition

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 $.ajax({
     url:'https://api.edamam.com/api/recipes/v2?type=public&q=fruity&app_id=113b4f65&app_key=210535346349c3454a01549b849ebdb0&dishType=Desserts&imageSize=REGULAR'
   }).then(
-    (data) => {
-     console.log(data);
+    (data1) => {
+     console.log(data1);
     },
-    (error) => {
-     console.log('bad request', error);
+    (error1) => {
+     console.log('bad request1', error1);
     }
   );
 
@@ -15,10 +15,10 @@ $.ajax({
   $.ajax({
     url:'https://api.spoonacular.com/recipes/716429/information?apiKey=e4fd76405420474aaaa447bc8cd20c73&includeIngredients=true'
   }).then(
-    (data) => {
-     console.log(data);
+    (data2) => {
+     console.log(data2);
     },
-    (error) => {
-     console.log('bad request', error);
+    (error2) => {
+     console.log('bad request2', error2);
     }
   );
